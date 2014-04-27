49e03b6628843e2167c31875ccb67850da1b39f0
yank555-lu <yank555.lu@gmail.com>
2014-04-27 13:40:02 +0200

    freezer: add new freezable helpers using freezer_do_not_count()
    Date	Wed, 1 May 2013 18:35:01 -0700
    
    Freezing tasks will wake up almost every userspace task from
    where it is blocking and force it to run until it hits a
    call to try_to_sleep(), generally on the exit path from the syscall
    it is blocking in.  On resume each task will run again, usually
    restarting the syscall and running until it hits the same
    blocking call as it was originally blocked in.
    
    To allow tasks to avoid running on every suspend/resume cycle,
    this patch adds additional freezable wrappers around blocking calls
    that call freezer_do_not_count().  Combined with the previous patch,
    these tasks will not run during suspend or resume unless they wake
    up for another reason, in which case they will run until they hit
    the try_to_freeze() in freezer_count(), and then continue processing
    the wakeup after tasks are thawed.  This patch also converts the
    existing wait_event_freezable* wrappers to use freezer_do_not_count().
    
    Additional patches will convert the most common locations that
    userspace blocks in to use freezable helpers.
    
    Signed-off-by: Colin Cross <ccross@android.com>

--- include/linux/freezer.h
+++ include/linux/freezer.h
@@ -213,6 +213,26 @@ static inline long freezable_schedule_timeout_interruptible(long timeout)
 	return __retval;
 }
 
+/* Like schedule_timeout(), but should not block the freezer. */
+#define freezable_schedule_timeout(timeout)				\
+({									\
+	long __retval;							\
+	freezer_do_not_count();						\
+	__retval = schedule_timeout(timeout);				\
+	freezer_count();						\
+	__retval;							\
+})
+
+/* Like schedule_timeout_interruptible(), but should not block the freezer. */
+#define freezable_schedule_timeout_interruptible(timeout)		\
+({									\
+	long __retval;							\
+	freezer_do_not_count();						\
+	__retval = schedule_timeout_interruptible(timeout);		\
+	freezer_count();						\
+	__retval;							\
+})
+
 /* Like schedule_timeout_killable(), but should not block the freezer. */
 static inline long freezable_schedule_timeout_killable(long timeout)
 {
@@ -246,6 +266,17 @@ static inline int freezable_schedule_hrtimeout_range(ktime_t *expires,
 	freezer_count();
 	return __retval;
 }
+
+/* Like schedule_hrtimeout_range(), but should not block the freezer. */
+#define freezable_schedule_hrtimeout_range(expires, delta, mode)	\
+({									\
+	int __retval;							\
+	freezer_do_not_count();						\
+	__retval = schedule_hrtimeout_range(expires, delta, mode);	\
+	freezer_count();						\
+	__retval;							\
+})
+
 
 /*
  * Freezer-friendly wrappers around wait_event_interruptible(),
@@ -291,7 +322,7 @@ static inline int freezable_schedule_hrtimeout_range(ktime_t *expires,
 	__retval;							\
 })
 
-#define wait_event_freezable_exclusive(wq, condition)			\
+#define wait_event_freezable_exclusive(wq, condition)		\
 ({									\
 	int __retval;							\
 	freezer_do_not_count();						\
